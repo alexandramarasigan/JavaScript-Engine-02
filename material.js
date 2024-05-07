6cbaeb2808db9bb0e7021fdd7d188091d168cb09
alexandramarasigan <158787198+alexandramarasigan@users.noreply.github.com>
2024-05-07 11:20:37 +1000

    material class

---
+++ material.js
@@ -0,0 +1,8 @@
+export class Material {
+    constructor(name, density, restitution, color) {
+        this.name = name;
+        this.density = density;
+        this.restitution = restitution;
+        this.color = color;
+    }
+}
