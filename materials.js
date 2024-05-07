b4814519d1532b6355b11c67f658b645bda259f0
alexandramarasigan <158787198+alexandramarasigan@users.noreply.github.com>
2024-05-07 11:41:59 +1000

    materials class

---
+++ materials.js
@@ -0,0 +1,6 @@
+export const Materials = {
+    default: { density: 1, restitution: 0.2, fillColor: 'lightgrey' },
+    wood: { density: 0.8, restitution: 0.4, fillColor: 'saddlebrown' },
+    steel: { density: 7.8, restitution: 0.05, fillColor: 'grey' },
+    rubber: { density: 1.2, restitution: 0.9, fillColor: 'black' }
+};
